31cbc4655f835681eb14069080b1130e3d2b688c
Willian Oliveira <wijloc@gmail.com>
2020-10-15 12:09:32 -0300

    Ep.4 Inclusão do backend Refatorações

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,26 +1,41 @@
 //create map
-const map = L.map("mapid").setView([-27.2057496, -49.6582354], 15);
+const map = L.map("mapid").setView([-27.222633, -49.6455874], 15);
 
 //create and add tileLayer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //create icon
 const icon = L.icon({
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2],
 });
 
-//create popup overlay
-const popup = L.popup({
-  closeButton: false,
-  className: "map-popup",
-  minWidth: 240,
-  minHeight: 240,
-}).setContent(
-  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>'
-);
+function addMarker({id, name, lat, lng}){
 
-//create and add marker
-L.marker([-27.2057496, -49.6582354], { icon }).addTo(map).bindPopup(popup);
+  //create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg" </a>`
+  );
+
+  //create and add marker
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach(span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+  addMarker(orphanage)
+})
